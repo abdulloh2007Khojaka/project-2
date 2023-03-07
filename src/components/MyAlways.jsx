1dc7f9b793edfc12bc01d1b9e8f504c1fcaa680e
abdulloh2007Khojaka <alijonovabdulloh25012007@gmail.com>
2023-03-07 16:12:19 +0500

    always da p tegni qoshdim

--- src/components/MyAlways.jsx
+++ src/components/MyAlways.jsx
@@ -13,6 +13,7 @@ export default function MyAlways() {
                         <div className="col-sm-12 col-md-6 col-lg-6">
                             <p className="dolor">Lorem ipsum dolor</p>
                             <h1 className="saas">Always the real <br /> thing, always Saas</h1>
+                            <p>salom</p>
                             <p className="growing">And that number is growing every day. That <br /> means e-commerce is thriving.</p>
                             <button className="free">try for free</button>
                             <button className="free">try for free</button>
